a579b1d436558ad9d3dbdce3f7039a3db1669c50
Candeve <grandjeanemery@gmail.com>
2024-05-14 01:15:53 +0200

    Etape_1.1 / galerie fonctionnelle affichée avec la liste des travaux provenant du back-end

---
+++ FrontEnd/index.js
@@ -0,0 +1,59 @@
+// Fetches works data from API
+async function fetchData() {
+    try {
+        const response = await fetch("http://localhost:5678/api/works");
+
+        if (!response.ok) {
+            throw new Error('La requête a échoué');
+        }
+
+        const data = await response.json();
+
+        console.log(data); // Display the data in the console
+
+        return data; // Return the fetched data
+
+    } catch (error) { // Catch any error
+
+        console.error('Erreur pendant la requête fetch :', error);
+    }
+}
+
+// Create the HTML elements
+function createGallery(works) {
+    
+
+    for (let i = 0; i < works.length; i++) {
+        const project = works[i];
+
+
+        const galleryElement = document.querySelector('.gallery'); // Get the gallery element
+
+        const figureElement = document.createElement("figure"); // Create a figure element for every project
+
+        const imageElement = document.createElement("img"); // Create an image element for every project
+        imageElement.src = project.imageUrl;
+
+        const titleElement = document.createElement("figcaption"); // Create a figcaption element for every project
+        titleElement.innerHTML = project.title;
+
+        // Attach child elements to the figure element
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(titleElement);
+
+        // Attach the figure element to the gallery element
+        galleryElement.appendChild(figureElement);
+    }
+}
+
+// Fetch data and create the gallery
+async function initGallery() {
+    const worksData = await fetchData(); // Wait for the data to be fetched
+    if (worksData) {
+        createGallery(worksData); // Create the gallery with the fetched data
+    }
+};
+
+
+
+initGallery(); // Initialize the gallery
